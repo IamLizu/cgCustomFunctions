aec0fa2a2b2b0a4473c5d8a7e96ec40d15d87d8e
S M Mahmudul Hasan <thegeek@iamlizu.com>
2020-12-02 14:51:03 +0600

    Update funcs.cpp

--- quad/funcs.cpp
+++ quad/funcs.cpp
@@ -4,3 +4,14 @@ void quadHorzInt(int pT, int pB, int fixedP, int fixedQ) {
     glVertex2i(pT, fixedQ);
     glVertex2i(pB, fixedQ);
 }
+
+// float 
+void quadHorzFloat(float pT, float pB, float fixedP, float fixedQ) {
+    glBegin(GL_QUADS);
+    glVertex2f(pB, fixedP);
+    glVertex2f(pT, fixedP);
+    glVertex2f(pT, fixedQ);
+    glVertex2f(pB, fixedQ);
+    glEnd();
+}
+
